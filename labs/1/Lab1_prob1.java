f3831b694fc695d7c4d122e56b45c11f2acf8ebf
daynaeidle <dayna.eidle1@marist.edu>
2017-01-24 13:14:29 -0500

    Completed first problem in Lab 1.

---
+++ labs/1/Lab1_prob1.java
@@ -0,0 +1,17 @@
+/*
+* File: Lab1_prob1.java
+* Author: Dayna Eidle
+* Course: CMPT 220
+* Assignment: Lab 1
+* Due Date: 1/26/2017
+* Version: 1.8
+*/
+
+public class Lab1_prob1{
+  public static void main(String [] args){
+    System.out.println("      J     A     V     V    A     ");
+    System.out.println("      J    A A     V   V    A A    ");
+    System.out.println(" J    J   AAAAA     V V    AAAAA   ");
+    System.out.println("  J J    A     A     V    A     A  ");
+  }
+}
