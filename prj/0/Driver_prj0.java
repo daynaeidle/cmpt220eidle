f8c4bc55d3677699371cb361fce30a8cbee3bbd5
daynaeidle <dayna.eidle1@marist.edu>
2017-01-24 12:54:07 -0500

    Final Project 0 code uploaded.

---
+++ prj/0/Driver_prj0.java
@@ -0,0 +1,14 @@
+/*
+* File: Driver_prj0.java
+* Author: Dayna Eidle
+* Course: CMPT 220
+* Assignment: Project 0
+* Due Date: 1/26/2017
+* Version: 1.8
+*/
+
+public class Driver_prj0{
+  public static void main (String[] args){
+    System.out.println("Hello World!");
+  }
+}
