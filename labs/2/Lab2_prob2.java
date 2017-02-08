5601e2119e1b7a93f6dc6fc7786f060319b5f124
daynaeidle <dayna.eidle1@marist.edu>
2017-02-08 11:16:45 -0500

    Completed Problem 3.8 in Lab 2.

---
+++ labs/2/Lab2_prob2.java
@@ -0,0 +1,42 @@
+/*
+* File: Lab2_prob2.java
+* Author: Dayna Eidle
+* Course: CMPT 220
+* Assignment: Lab 2
+* Due Date: 2/9/2017
+* Version: 1.8
+*/
+
+import java.util.Scanner;
+
+public class Lab2_prob2{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    
+    int firstNum, secondNum, thirdNum;
+    int swap = 0; //place holder variable
+    
+    System.out.print("Please enter three integers: ");
+    firstNum = input.nextInt();
+    secondNum = input.nextInt();
+    thirdNum = input.nextInt();
+    
+    while (firstNum > secondNum || firstNum > thirdNum || secondNum > thirdNum){
+      if (secondNum < firstNum){
+        swap = secondNum;
+        secondNum = firstNum;
+        firstNum = swap;
+      }
+      if (thirdNum < secondNum){
+        swap = thirdNum;
+        thirdNum = secondNum;
+        secondNum = swap;
+      }
+    }
+    
+    System.out.println("First: " + firstNum);
+    System.out.println("Second: " + secondNum);
+    System.out.println("Third: " + thirdNum);
+
+  }
+}
